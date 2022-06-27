9c1d3163563680eb1ba785b3a7196c0516addb07
abc <swati@solulab.com>
2022-06-27 16:42:50 +0530

    changed script file

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -24,3 +24,7 @@ main()
 		console.error(error)
 		process.exit(1)
 	})
+
+// 	ERC20 Token deployed to: 0x5d9Ab62c24b1DCd50e59CC5713b0Da9f798a9420
+// Timelock deployed to: 0x5835D7A2B7F172bF63D1d025F676AaEEe7De8Db5
+// Goverance token deployed to: 0xe18C679f5C859B2C855FdB4a0cD13149aB63bCa2
